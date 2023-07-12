1bab3246f41e9cd548c756096863a9197174a417
Esteban Muruzabal <estebanmuruzabal@gmail.com>
2023-07-11 20:10:44 -0400

    fixes

--- server/src/utils/logsUtils.ts
+++ server/src/utils/logsUtils.ts
@@ -2,14 +2,27 @@ import moment from "moment";
 import { ISoilHumiditySettings } from "../lib/types";
 
 export const logTimeStamp = (soilHumiditySetting: ISoilHumiditySettings, currentSoilHumidity: number) => {
-    // we only log timestamp if there has pass 5 mins from the latest timestamp currently
-    const lastTimeStamp = soilHumiditySetting?.logs[soilHumiditySetting?.logs?.length]?.timestamp;
-    const lastTimeStampMoment = moment(new Date(lastTimeStamp).toLocaleString('en-US', { timeZone: 'America/Argentina/Buenos_Aires' })).format('hh:mm:ss');
-    const currentTimeMoment = moment(new Date().toLocaleString('en-US', { timeZone: 'America/Argentina/Buenos_Aires' }));    
-    const lastTimestampInMins = currentTimeMoment?.diff(lastTimeStampMoment, 'minutes');
+    const currentTimeMoment = moment(new Date());
 
-    if (lastTimestampInMins < 5 && soilHumiditySetting?.logs.length !== 0) return soilHumiditySetting;
+    if (soilHumiditySetting?.logs?.length > 0) {
+        const lastTimeStamp = soilHumiditySetting?.logs[soilHumiditySetting?.logs?.length]?.timestamp;
+        const lastTimeStampMoment = moment(new Date(lastTimeStamp));
+        const lastTimestampInMins = currentTimeMoment?.diff(lastTimeStampMoment, 'minutes');
 
+        if (lastTimestampInMins < 5 && soilHumiditySetting?.logs.length !== 0) return soilHumiditySetting;
+    }
+
+//         // we only log timestamp if there has pass 5 mins from the latest timestamp currently
+//     const lastTimeStamp = soilHumiditySetting?.logs[soilHumiditySetting?.logs?.length]?.timestamp || currentTimeMoment;
+//     console.log('lastTimeStamp', lastTimeStamp)
+// // @ts-ignore
+//     const lastTimeStampMoment = moment(new Date(lastTimeStamp|| currentTimeMoment));
+//     console.log('lastTimeStampMoment',lastTimeStampMoment)
+//     const lastTimestampInMins = currentTimeMoment?.diff(lastTimeStampMoment, 'minutes');
+// console.log('lastTimestampInMins', lastTimestampInMins)
+//     if (lastTimestampInMins < 5 && soilHumiditySetting?.logs.length !== 0) return soilHumiditySetting;
+
+    soilHumiditySetting.logs = [];
     soilHumiditySetting?.logs.push({
         humidity: currentSoilHumidity,
         timestamp: new Date().toLocaleString('en-US', { timeZone: 'America/Argentina/Buenos_Aires' })
